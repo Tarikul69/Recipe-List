808b79fdf5dd9c4e4bbfadfa7960e7a071b71f27
tarikulabir <tarikulabir931@gmail.com>
2024-01-17 01:14:37 +0600

    Controller

--- lib/components/rout.dart
+++ lib/components/rout.dart
@@ -3,6 +3,10 @@ import 'package:recipe_list_app/view/splashscreen.dart';
 
 class Approutes {
   static List<GetPage> routes() => [
-    GetPage(name: "/", page: ()=> splashscreen(), transition: Transition.rightToLeft,),
+    GetPage(
+      name: "/",
+      page: ()=> splashscreen(),
+      transition: Transition.rightToLeft,
+    ),
   ];
 }
